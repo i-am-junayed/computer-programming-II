2ba755671fd3f6c011266c1c11ab1ed0dd8d347c
i-am-junayed <junayed.ndc16@gmail.com>
2025-05-22 22:17:50 +0600

    Update problem_1.py

--- loop/problem_1.py
+++ loop/problem_1.py
@@ -1,1 +1,7 @@
+# 1.  **Print Numbers 1 to N:**
+#      Write a Python program that asks the user for an integer `N` and then prints all integers from 1 up to `N` (inclusive), 
+#      each on a new line.
 
+number = int(input("Enter a number : ")
+for i in range(1, number+1, 1):
+    print(i)
